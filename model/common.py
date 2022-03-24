cfc978721a6b909aff2b20e647df7dc8ce18bfa0
JadeForest <1113480910@qq.com>
2022-03-24 21:21:00 +0800

    code refined

--- model/common.py
+++ model/common.py
@@ -1,13 +1,7 @@
-import os
 from collections import OrderedDict
 
-import numpy as np
-import skimage
 import torch
 import torch.nn.functional as F
-from PIL import Image
-from clip import tokenize
-from matplotlib import pyplot as plt
 from torch import nn
 
 
@@ -242,72 +236,3 @@ class VisionTransformer(nn.Module):
         return x
 
 
-def generate_figure(similarity, texts, images):
-    count = len(texts)
-    figure = plt.figure(figsize=(16, 16))
-    plt.imshow(similarity, vmin=0.1, vmax=0.3)
-    plt.colorbar()
-    plt.yticks(range(count), texts, fontsize=18)
-    plt.xticks([])
-    for i, image in enumerate(images):
-        plt.imshow(image, extent=(i - 0.5, i + 0.5, -1.6, -0.6), origin="lower")
-    for x in range(similarity.shape[1]):
-        for y in range(similarity.shape[0]):
-            plt.text(x, y, f"{similarity[y, x]:.2f}", ha="center", va="center", size=12)
-
-    for side in ["left", "top", "right", "bottom"]:
-        plt.gca().spines[side].set_visible(False)
-
-    plt.xlim([-0.5, count - 0.5])
-    plt.ylim([count + 0.5, -2])
-
-    plt.title("Cosine similarity between text and image features", size=20)
-    return figure
-
-
-def add_similarity(model, tb_write, epoch, **kwargs):
-    descriptions = {
-        "page": "a page of text about segmentation",
-        "chelsea": "a facial photo of a tabby cat",
-        "astronaut": "a portrait of an astronaut with the American flag",
-        "rocket": "a rocket standing on a launchpad",
-        "motorcycle_right": "a red motorcycle standing in a garage",
-        "camera": "a person looking at a camera on a tripod",
-        "horse": "a black-and-white silhouette of a horse",
-        "coffee": "a cup of coffee on a saucer"
-    }
-
-    original_images = []
-    images = []
-    texts = []
-    plt.figure(figsize=(16, 16))
-
-    for filename in [filename for filename in os.listdir(skimage.data_dir) if
-                     filename.endswith(".png") or filename.endswith(".jpg")]:
-        name = os.path.splitext(filename)[0]
-        if name not in descriptions:
-            continue
-        image = Image.open(os.path.join(skimage.data_dir, filename)).convert("RGB")
-        original_images.append(image)
-        images.append(model.teacher.preprocess(image))
-        texts.append(descriptions[name])
-    image_input = torch.tensor(np.stack(images), device=kwargs['device'])
-    text_tokens = tokenize([desc for desc in texts]).to(kwargs['device'])
-
-    image_features, tea_image_features = model(image_input)
-    text_features = model.teacher.encode_text(text_tokens).float()
-
-    image_features = image_features / image_features.norm(dim=-1, keepdim=True)
-    tea_image_features = tea_image_features / tea_image_features.norm(dim=-1, keepdim=True)
-    text_features = text_features / text_features.norm(dim=-1, keepdim=True)
-
-    similarity = (text_features @ image_features.T).softmax(dim=-1).cpu().numpy()
-    tea_similarity = (text_features @ tea_image_features.T).softmax(dim=-1).cpu().numpy()
-    similarity100 = (100 * text_features @ image_features.T).softmax(dim=-1).cpu().numpy()
-    tb_write.add_figure('Similarity/student', generate_figure(similarity, texts, original_images), epoch)
-    tb_write.add_figure('Similarity/teacher', generate_figure(tea_similarity, texts, original_images), epoch)
-    tb_write.add_figure('Similarity/100 * student', generate_figure(similarity100, texts, original_images), epoch)
-
-    plt.clf()
-
-    plt.close("all")
