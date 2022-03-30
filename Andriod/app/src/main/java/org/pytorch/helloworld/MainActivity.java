2cd9f0e0d5042b497a78408150749c39caca4192
unknown <623181268@qq.com>
2022-03-30 15:00:54 +0800

    Andriod_0_5

--- Andriod/app/src/main/java/org/pytorch/helloworld/MainActivity.java
+++ Andriod/app/src/main/java/org/pytorch/helloworld/MainActivity.java
@@ -20,70 +20,121 @@ import org.pytorch.torchvision.TensorImageUtils;
 import org.pytorch.MemoryFormat;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.pytorch.helloworld.Tokenizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+  public static float[] TORCHVISION_NORM_MEAN_RGB = new float[] {0.481f, 0.458f, 0.408f};
+  public static float[] TORCHVISION_NORM_STD_RGB = new float[] {0.269f, 0.261f, 0.276f};
+
+
+
+  public MainActivity() throws IOException {
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     //继承onCreate函数
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.main_activaty_ablayerout);
 
     //定义bitmap和module两个变量
+    List<Bitmap> ImageSet = new ArrayList<Bitmap>();
     Bitmap bitmap = null;
-    Module module = null;
     Module module_image = null;
     Module module_text = null;
+    Tokenizer Simple_tokenize = null;
     try {
       // creating bitmap from packaged into app android asset 'image.jpg',
       // app/src/main/assets/image.jpg 将jpg图片转换成bitmap
       /* 1-读取图片 */
       //Image image=getAssets().open("image.jpg");
       bitmap = BitmapFactory.decodeStream(getAssets().open("test2.jpg"));
-
+      ImageSet.add(BitmapFactory.decodeStream(getAssets().open("test1.jpg")));
+      ImageSet.add(BitmapFactory.decodeStream(getAssets().open("test2.jpg")));
+      ImageSet.add(BitmapFactory.decodeStream(getAssets().open("test3.jpg")));
       // loading serialized torchscript module from packaged into app android asset model.pt,
       // app/src/model/assets/model.pt
       /* 2-读取模型 */
-      module = LiteModuleLoader.load(assetFilePath(this, "model.pt"));
-      //读取image模型
-      module_image = LiteModuleLoader.load(assetFilePath(this, "our_image_clip_cpu.ptl"));
-      //读取text模型
-      //module_text = LiteModuleLoader.load(assetFilePath(this, "our_text_clip.ptl"));
-      //module_image = LiteModuleLoader.load(assetFilePath(this, "clip.ptl"));
+      //module = LiteModuleLoader.load(assetFilePath(this, "model.pt"));
+      Simple_tokenize = new Tokenizer(assetFilePath(this, "vocab.txt"));
+      //读取image-text模型
+      module_image = LiteModuleLoader.load(assetFilePath(this, "image_encode.ptl"));
+      module_text = LiteModuleLoader.load(assetFilePath(this, "text_encode.ptl"));
     } catch (IOException e) {
       Log.e("PytorchHelloWorld", "Error reading assets", e);
       finish();
     }
 
     // showing image on UI
-    ImageView imageView = findViewById(R.id.image);
-    imageView.setImageBitmap(bitmap);
-
+    //ImageView imageView = findViewById(R.id.imageView2);
+    //imageView.setImageBitmap(bitmap);
+    TextView textView4 = findViewById(R.id.textView4);
+    textView4.setText("lebron james");
     // preparing input tensor
-    /* 3-将bitmap图片转换成tensor并进行预处理（需与原模型输入保持一直，可以保证准确率） */
+    /* 3.1-将String文本转换进行预处理*/
+
+    Tensor text_token = tokenize("lebron james", 77, Simple_tokenize);
+
+    /* 3.2-将bitmap图片转换成tensor并进行预处理（需与原模型输入保持一直，可以保证准确率） */
     final int INPUT_TENSOR_WIDTH = 224;
     final int INPUT_TENSOR_HEIGHT = 224;
-    bitmap=resizeImage(bitmap,INPUT_TENSOR_WIDTH,INPUT_TENSOR_HEIGHT);
+    //List<Tensor> TensorSet = new ArrayList<Tensor>();
+    Tensor t_tensor = null;
+    float[][] ImageFeatureSet = new float[ImageSet.size()][];
+    for (int i = 0; i < ImageSet.size(); i++) {
+      ImageSet.set(i, resizeImage(ImageSet.get(i), INPUT_TENSOR_WIDTH, INPUT_TENSOR_HEIGHT));
+      t_tensor = TensorImageUtils.bitmapToFloat32Tensor(ImageSet.get(i), TORCHVISION_NORM_MEAN_RGB, TORCHVISION_NORM_STD_RGB, MemoryFormat.CHANNELS_LAST);
+      //TensorSet.add(TensorImageUtils.bitmapToFloat32Tensor(ImageSet.get(i),TORCHVISION_NORM_MEAN_RGB, TORCHVISION_NORM_STD_RGB, MemoryFormat.CHANNELS_LAST));
+
+      t_tensor = module_image.forward(IValue.from(t_tensor)).toTensor();
+      ImageFeatureSet[i] = t_tensor.getDataAsFloatArray();
+    }
+
+    bitmap = resizeImage(bitmap, INPUT_TENSOR_WIDTH, INPUT_TENSOR_HEIGHT);
     final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
-        TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB, MemoryFormat.CHANNELS_LAST);
+            TORCHVISION_NORM_MEAN_RGB, TORCHVISION_NORM_STD_RGB, MemoryFormat.CHANNELS_LAST);
 
     //final Tensor inputTensor = TensorImageUtils.imageYUV420CenterCropToFloat32Tensor(bitmap,
-     //       TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB, MemoryFormat.CHANNELS_LAST);
+    //       TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB, MemoryFormat.CHANNELS_LAST);
     // running the model
     /* 4-将图片输入模型 */
-    final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();//forward(IValue.from(inputTensor)).toTensor();
+    //final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();//forward(IValue.from(inputTensor)).toTensor();
     final Tensor outputImageFeature = module_image.forward(IValue.from(inputTensor)).toTensor();
+    final Tensor outputTextFeature = module_text.forward(IValue.from(text_token)).toTensor();
     // getting tensor content as java array of floats
-    final float[] scores = outputTensor.getDataAsFloatArray();
-    final float[] ImageFeatures = outputImageFeature.getDataAsFloatArray();
+    //final float[] scores = outputTensor.getDataAsFloatArray();
+    final float[] ImageFeature = outputImageFeature.getDataAsFloatArray();
+    final float[] TextFeature = outputTextFeature.getDataAsFloatArray();
     // searching for the index with maximum score
     /* 5-比较输出最大可能的分类 */
+    float[] Similarity = ConSimilarity(ImageFeatureSet, TextFeature);
+    TextView textView = findViewById(R.id.textView);
+    TextView textView2 = findViewById(R.id.textView2);
+    TextView textView3 = findViewById(R.id.textView3);
+
+    ImageView imageView3 = findViewById(R.id.imageView3);
+    ImageView imageView4 = findViewById(R.id.imageView4);
+    ImageView imageView5 = findViewById(R.id.imageView5);
+    int[] Index = Arraysort(Similarity);
+    TextView textView1 = findViewById(R.id.text);
+    textView.setText("Top[" + 0 + "]:" + String.format("%.2f", Similarity[0]));
+    textView2.setText("Top[" + 1 + "]:" + String.format("%.2f", Similarity[1]));
+    textView3.setText("Top[" + 2 + "]:" + String.format("%.2f", Similarity[2]));
+
+    imageView3.setImageBitmap(ImageSet.get(Index[0]));
+    imageView4.setImageBitmap(ImageSet.get(Index[1]));
+    imageView5.setImageBitmap(ImageSet.get(Index[2]));
+    /*
     float maxScore = -Float.MAX_VALUE;
     int maxScoreIdx = -1;
     int[] max_id = new int[5];
@@ -101,6 +152,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView = findViewById(R.id.text);
     textView.setText("                                 前五分类预测\n"+className[0]+"\n"+className[1]+"\n"+className[2]+"\n"+className[3]+"\n"+className[4]+"\n");
     //textView.setText("习近平");
+
+     */
   }
 
   /**
@@ -157,13 +210,87 @@ public class MainActivity extends AppCompatActivity {
     return resizedBitmap;
   }
 
-  public Tensor ConSimilarity(float[][] ImageFeatures,float[][] ImageFeatures)
+  public float[] ConSimilarity(float[][] ImageFeatures,float[] TextFeatures)
   {
-     for(int i=0;i<10;i++)
-     {
-
-     }
-    ImageFeatures*ImageFeatures;
+    float [] Similarity = new float[ImageFeatures.length];
+    for(int i=0;i<ImageFeatures.length;i++)
+    {
+      for(int j=0;j<ImageFeatures[i].length;j++)
+      {
+        Similarity[i]+=ImageFeatures[i][j]*TextFeatures[j];
+      }
+    }
+    float sum=0;
+    for(int i=0;i<Similarity.length;i++)
+    {
+      Similarity[i]= (float) Math.exp(100*Similarity[i]);
+      sum+=Similarity[i];
+    }
+    for(int i=0;i<Similarity.length;i++)
+    {
+      Similarity[i]/=sum;
+    }
+    return Similarity;
   }
 
+  public static int[] Arraysort(float[]arr)
+  {
+    //double[] arr = {5.5,2,66,3,7,5};
+    float temp;
+    int index;
+    int k=arr.length;
+    int[]Index= new int[k];
+    for(int i=0;i<k;i++)
+    {
+      Index[i]=i;
+    }
+
+    for(int i=0;i<arr.length;i++)
+    {
+      for(int j=0;j<arr.length-i-1;j++)
+      {
+        if(arr[j]<arr[j+1])
+        {
+          temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+
+          index=Index[j];
+          Index[j] = Index[j+1];
+          Index[j+1] = index;
+        }
+      }
+    }
+    return Index;
+  }
+
+  public Tensor tokenize(String text,int context_length,Tokenizer Simple_tokenize)
+  {
+
+    int startToken=Simple_tokenize.encoder.get("<|startoftext|>");
+    int endToken=Simple_tokenize.encoder.get("<|endoftext|>");
+
+    //int startToken=Tokenizer.encode();
+    //int endToken=49407;
+
+    //int [] textToken=Simple_tokenize.encode(text);
+    int [] textToken=Tokenizer.encode(text);
+    int [] allToken=new int[context_length];
+    for(int i=0;i<context_length;i++)
+    {
+      if(i==0)
+      {
+        allToken[i]=startToken;
+      }
+      else if(i<textToken.length+1 && i>0)
+      {
+        allToken[i]=textToken[i-1];
+      }
+      else if(i==textToken.length+1) {
+        allToken[i] = endToken;
+      }
+    }
+    Tensor inputTensor = Tensor.fromBlob(allToken, new long[]{1, context_length});
+    return inputTensor;
+  }
 }
---
+++ Andriod/app/src/main/java/org/pytorch/helloworld/Tokenizer.java
@@ -0,0 +1,207 @@
+package org.pytorch.helloworld;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Tokenizer {
+
+    static TokenTool tokenTool;
+    //TokenTool tokenTool;
+    static Map<Integer, Character> encode_map;
+    static List<String[]> merges;
+    static List<String> vocab;
+    static Map<String, Integer> encoder;
+    static Map<String[], Integer> bpe_ranks;
+    static Map<String, String> cache;
+    static String pat;
+
+    /*
+    public Tokenizer(String filename) throws FileNotFoundException {
+        //构建字典
+        tokenTool=new TokenTool();
+        File file = new File(filename);
+        Scanner s = new Scanner(file);
+        encode_map = new HashMap<>();
+        merges=new ArrayList<>();
+        vocab=new ArrayList<>();
+        encoder=new HashMap<>();
+        bpe_ranks=new HashMap<>();
+        cache=new HashMap<>();
+
+        while(s.hasNext())
+        {
+            merges.add(s.nextLine().split(" "));
+
+        }
+        merges=merges.subList(1,49152-256-2+1);
+        encode_map=tokenTool.bytes_to_unicode();
+        Generate_vocab();
+
+        for(int i=0;i<vocab.size();i++)
+        {
+            encoder.put(vocab.get(i),i);
+        }
+        for(int i=0;i<merges.size();i++)
+        {
+            bpe_ranks.put(merges.get(i),i);
+        }
+        cache.put("<|startoftext|>","<|startoftext|>");
+        cache.put("<|endoftext|>","<|endoftext|>");
+    }
+    */
+    public Tokenizer(String filename) throws FileNotFoundException {
+        //构建字典
+        tokenTool=new TokenTool();
+        File file = new File(filename);
+        Scanner s = new Scanner(file);
+        encode_map = new HashMap<>();
+        merges=new ArrayList<>();
+        vocab=new ArrayList<>();
+        encoder=new HashMap<>();
+        bpe_ranks=new HashMap<>();
+        cache=new HashMap<>();
+
+        while(s.hasNext())
+        {
+            vocab.add(s.nextLine());
+            //merges.add(s.nextLine().split(" "));
+
+        }
+        //merges=merges.subList(1,49152-256-2+1);
+        //encode_map=tokenTool.bytes_to_unicode();
+        //Generate_vocab();
+
+        for(int i=0;i<vocab.size();i++)
+        {
+            encoder.put(vocab.get(i),i);
+        }
+        /*
+        for(int i=0;i<merges.size();i++)
+        {
+            bpe_ranks.put(merges.get(i),i);
+        }
+
+        cache.put("<|startoftext|>","<|startoftext|>");
+        cache.put("<|endoftext|>","<|endoftext|>");*/
+    }
+    /*
+    void bpe()
+    {
+
+    }
+    */
+    void Generate_vocab()
+    {
+        List<Character> t_vocab= new ArrayList<Character>(encode_map.values());
+        List<String> tt_vocab= new ArrayList<String>();
+
+        for(Character i : t_vocab)
+        {
+            vocab.add(String.valueOf(i));
+        }
+        for(String i : vocab)
+        {
+            tt_vocab.add(i+"</w>");
+        }
+        vocab.addAll(tt_vocab);
+        for(String[] i : merges)
+        {
+            String t=new String();
+            for(String j: i)
+            {
+                t=t+j;
+            }
+            vocab.add(t);
+        }
+        vocab.add("<|startoftext|>");
+        vocab.add("<|endoftext|>");
+    }
+
+    static int[] encode(String text)
+    {
+        text = tokenTool.whitespace_clean(text).toLowerCase();
+        String[] texts=text.split(" ");
+        int [] bpe_token=new int[texts.length];
+        for(int i=0;i<texts.length;i++)
+        {
+            bpe_token[i]=encoder.get(texts[i]+"</w>");
+        }
+        return bpe_token;
+    }
+}
+
+class TokenTool{
+    Map<Integer, Character> bytes_to_unicode()
+    {
+        Map<Integer, Character> encode_map=new HashMap<>();
+        List<Integer> bs = new ArrayList<Integer>();
+        for(int i=(int)('!');i<=(int)('~');i++)
+        {
+            bs.add(i);
+        }
+        for(int i=(int)('¡');i<=(int)('¬');i++)
+        {
+            bs.add(i);
+        }
+        for(int i=(int)('®');i<=(int)('ÿ');i++)
+        {
+            bs.add(i);
+        }
+        List<Integer> cs=new ArrayList<>();
+        for(Integer i:bs)
+        {
+            cs.add(i);
+        }
+        int n=0;
+        for(int i=0;i<256;i++)
+        {
+            if(!bs.contains(i))
+            {
+                bs.add(i);
+                cs.add(256+n);
+                n+=1;
+            }
+        }
+        List<Character> Cs = new ArrayList<Character>();
+        for(int i=0;i<cs.size();i++)
+        {
+            Cs.add((char)((int)(cs.get(i))));
+        }
+        for(int i=0;i<Cs.size();i++)
+        {
+            encode_map.put(bs.get(i),Cs.get(i));
+        }
+        return encode_map;
+    }
+    /*
+    def get_pairs(word):
+            """Return set of symbol pairs in a word.
+    Word is represented as tuple of symbols (symbols being variable-length strings).
+    """
+    pairs = set()
+    prev_char = word[0]
+            for char in word[1:]:
+            pairs.add((prev_char, char))
+    prev_char = char
+    return pairs
+
+
+    def basic_clean(text):
+    text = ftfy.fix_text(text)
+    text = html.unescape(html.unescape(text))
+            return text.strip()
+
+*/
+    String whitespace_clean(String text)
+    {
+        text.replaceAll("\\s+"," ");
+        return text;
+    }
+
+
+}
